373d1f2b499933e40345fa5aec855de60e5cb4ec
Hugh Bowie IV <hughbowie@me.com>
2021-05-16 17:52:28 -0400

    ✅ optimized code

--- index.js
+++ index.js
@@ -1,159 +1,71 @@
-
-const inquirer = require('inquirer');
+// TODO: Include packages needed for this application
 const fs = require('fs');
-const genrateMarkdown = require('./utils/generateMarkdown');
-
+const inquirer = require('inquirer');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
+const writeFileAsync = util.promisify(fs.writeFile);
 // TODO: Create an array of questions for user input
-const questions = () => {
+const promptUser = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'Whats your full name? (Required)',
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('This app needs your name');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
             name: 'title',
-            message: 'What is the title of this application? (Required)',
-            validate: (titleInput) => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please title this application');
-                    return false;
-                }
-            }
+            message: 'What is the title of this app?',
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a brief describe for this application: (Required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Any description is necessary');
-                    return false;
-                }
-            }
+            message: 'Describe how your application works.',
         },
         {
             type: 'input',
-            name: 'install',
-            message: 'What installations are needed for this application? (Required)',
-            validate: installInput => {
-                if (installInput) {
-                    return true;
-                } else {
-                    console.log('Some installation instructions are necessary');
-                    return false;
-                }
-            }
+            name: 'installation',
+            message: 'Please describe how to install your app.',
         },
         {
             type: 'input',
             name: 'useage',
-            message: 'Please write useage instructions or examples: (Required)',
-            validate: useageInput => {
-                if (useageInput) {
-                    return true;
-                } else {
-                    console.log('Some useage instructions are necessary');
-                    return false;
-                }
-            }
+            message: 'Describe what this app is used for.',
         },
         {
             type: 'input',
-            name: 'contribute',
-            message: 'Please advise best ways for developers to contribute to this application (Required)',
-            validate: contributeInput => {
-                if (contributeInput) {
-                    return true;
-                } else {
-                    console.log('Please input some contribution tips for other developers to contribute');
-                    return false;
-                }
-            }
+            name: 'tests',
+            message: 'How can others test your app',
         },
         {
             type: 'input',
-            name: 'test',
-            message: 'Please advise testing instructions for this application (Required)',
-            validate: testInput => {
-                if (testInput) {
-                    return true;
-                } else {
-                    console.log('Anything at all will suffice');
-                    return false;
-                }
-            }
+            name: 'contribution',
+            message: 'How can other developers contribute to this project'
+
         },
         {
             type: 'list',
             name: 'license',
-            message: 'Please choose a license for this application (Required)',
-            choices: ['Apache', 'Boost', 'GNU GPLv3', 'MIT', 'Mozilla'],
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub username (Required)',
-
+            message: 'What license do you use?',
+            choices: ['Apache', 'GNU GPLv3', 'MIT', 'Mozilla']
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Enter your email address (Required)',
-
+            message: 'What is your email?',
         },
         {
-            type: 'checkbox',
-            name: 'languages',
-            message: 'What did you this project with? (Check all that apply)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
         },
+
     ]);
 };
 
-questions()
-    .then((readMeData) => {
-        return generateMarkdown(readMeData);
-    })
-    .then((md) => {
-        return writeToFile('./dist/README.md', md);
-    })
-    .catch((err) => {
-        console.log(`Error: ${err}`);
-    })
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(fileName, data, (err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve('README.md has been created with a little magic.');
-        });
-    });
-}
+const init = () => {
+    promptUser()
+        .then((data) => writeFileAsync(`${data.title}.md`, generateMarkdown(data)))
+        .then(() => console.log('README.md File Created.'))
+        .catch((err) => console.error(err));
+};
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((data) => {
-        writeToFile('README.md', generateMarkdown(data));
-    });
-}
-
-// Function call to initialize app
 init();
